66634996884aba8462e9c366bf6a49378b0d6944
vpekane <vpekane@student.wethinkcode.co.za>
2023-01-23 21:54:13 +0200

    defined configurations for django admin to suit custom user model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from . import models
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 # Register your models here.
 
@@ -38,5 +39,61 @@ class ProductImageAdmin(admin.ModelAdmin):
     
     def product_name(self, obj):
         return obj.product.name
+
+@admin.register(models.User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("email", "password")
+            }
+        ),
+        (
+            "Personal info",
+            {
+                "fields": ("first_name", "last_name")
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_persmissions",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": ("last_login", "date_joined")
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide"),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name"
+    )
+    ordering = ("email",)
 
 admin.site.register(models.ProductImage, ProductImageAdmin)
