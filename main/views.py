956414060070bc63a4d742262e22dd9f0cfba011
vpekane <vpekane@student.wethinkcode.co.za>
2023-01-24 23:37:06 +0200

    added view to handle signup process

--- main/views.py
+++ main/views.py
@@ -5,6 +5,11 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
 from main import models
+import logging
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 class ContactUsView(FormView):
@@ -35,3 +40,28 @@ class ProductListView(ListView):
             products = models.Product.object.active()
 
         return products.order_by("name")
+
+class SignupView(FormView):
+    template_name = "signup.html"
+    form_class = forms.UserCreationForm
+
+    def get_success_url(self) -> str:
+        redirect_to = self.request.GET.get("next", "/")
+        return redirect_to
+
+    def form_valid(self, form) -> HttpResponse:
+        response = super().form_valid(form)
+        form.save()
+        email = form.cleaned_data.get("email")
+        raw_password = form.cleaned_data.get("password1")
+        logger.info(
+            "New signup for email=%s through SignupView", email
+        )
+        user = authenticate(email=email, password=raw_password)
+        login(self.request, user)
+
+        form.send_mail()
+        messages.info(
+            self.request, "You signed up successfully."
+        )
+        return response
