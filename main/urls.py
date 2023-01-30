b2b4b2b99018a700eb4f3e4a576d7bc844a520b0
vpekane <vpekane@student.wethinkcode.co.za>
2023-01-30 19:20:16 +0200

    created new url path

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,8 @@ from main import views
 from django.conf import settings
 from django.conf.urls.static import static
 from main import models
+from django.contrib.auth import views as auth_views
+from main import forms
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
@@ -13,7 +15,8 @@ urlpatterns = [
     path("products/<slug:tag>/", views.ProductListView.as_view(), name="products"),
     path("product/<slug:slug>/",
          DetailView.as_view(model=models.Product, template_name="product_detail.html"),
-         name="product"
-        ),
+         name="product"),
     path("signup/", views.SignupView.as_view(), name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html",
+         form_class=forms.AuthenticationForm,), name="login",),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
