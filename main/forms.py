fd041af2bde68cf37b22801909dee65f1c5ce959
vpekane <vpekane@student.wethinkcode.co.za>
2023-01-30 19:22:28 +0200

    added authentication form for user login

--- main/forms.py
+++ main/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from django.core.mail import send_mail
 import logging
 from django.contrib.auth.forms import (
-    UserCreationForm as DjangoUserCreationForm
+    UserCreationForm as DjangoUserCreationForm,
+    UsernameField
 )
 from . import models
-from django.contrib.auth.forms import UsernameField
+from django.contrib.auth import authenticate
 
 logger = logging.getLogger(__name__)
 
@@ -47,3 +48,35 @@ class UserCreationForm(DjangoUserCreationForm):
             [self.cleaned_data["email"]],
             fail_silently=True
         )
+
+class AuthenticationForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(
+        strip=False, widget=forms.PasswordInput
+    )
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user = None
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        if email is not None and password:
+            self.user = authenticate(
+                self.request, email=email, password=password
+            )
+
+            if self.user is None:
+                raise forms.ValidationError(
+                    "Invalid email/password combination"
+                )
+            logger.info(
+                "Authentication successful for email=%s", email
+            )
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user
