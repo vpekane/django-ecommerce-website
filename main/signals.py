1fe71d7bef74a7e930a8c271676ba0c7f5150724
vpekane <vpekane@student.wethinkcode.co.za>
2023-02-08 20:25:16 +0200

    added signal to merge anonymous user basket with logged in user basket

--- main/signals.py
+++ main/signals.py
@@ -4,7 +4,8 @@ from PIL import Image
 from django.core.files.base import ContentFile
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import ProductImage
+from django.contrib.auth.signals import user_logged_in
+from .models import ProductImage, Basket
 
 THUMBNAIL_SIZE = (300, 300)
 
@@ -31,3 +32,26 @@ def generate_thumbnail(sender, instance, **kwargs):
         save=False,
     )
     # temp_thumb.save()
+
+@receiver(user_logged_in)
+def merger_baskets_if_found(sender, user, request, **kwargs):
+    anonymous_basket = getattr(request, "basket", None)
+    if anonymous_basket:
+        try:
+            loggedin_basket = Basket.objects.get(
+                user=user, status=Basket.OPEN
+            )
+            for line in anonymous_basket.basketline_set.all():
+                line.basket = loggedin_basket
+                line.save()
+            anonymous_basket.delete()
+            request.basket = loggedin_basket
+            logger.info(
+                "Merged basket to id %d", loggedin_basket.id
+            )
+        except Basket.DoesNotExist:
+            anonymous_basket.user = user
+            anonymous_basket.save()
+            logger.info(
+                "Assigned user to basket id %d", anonymous_basket.id
+            )
