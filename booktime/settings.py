fb496347a65eec054f0f1ec05dde2edb84b04190
vpekane <vpekane@student.wethinkcode.co.za>
2023-02-12 18:21:56 +0200

    minor update

--- booktime/settings.py
+++ booktime/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'main/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 
